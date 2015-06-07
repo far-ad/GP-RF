123b01cb23a3538cc36c24fbe22d9287ae3dff4b
Jack1102 <dragomir.nikolic1987@gmail.com>
2015-06-07 15:17:17 +0200

    added cov and mean

--- Code/ImageConvert.m
+++ Code/ImageConvert.m
@@ -31,53 +31,73 @@ cd('..');
 
 %added for calculation for cov and  mean
 
-cd('desk_1/desk');
+cd('resizeobjects');
 Filesb = dir(strcat('*.png'));
 
-length(Filesb)
-i=2; j=2;
-Filesb{i}(j).name;
-Filesb(i).name
-A=zeros(); map=[];
-newCat=[];
+
+
+
+
+A=zeros(); map=[]; newCAP=[];
+newCat=[]; newcoffee_mug=[]; newsoda_soda_can=[]; newsoda_bowl=[]; newsoda_flashlight=[];
 for i = 1: length(Filesb)
-    %cap25 till cap65
-    if i<42
-        [A,map] = imread(Filesb(i).name);
+    %cap
+    if i<43 && i>1
+        [A,map] = imread(char(Filesb(i,1).name));
         newVector=reshape(A,[size(A,1)*size(A,2)*size(A,3), 1]);
-        newCAP= [newCat;newVector];
+        newCAP= [newCAP,newVector];
     end
     %76 elements of coffee_mug
-    if i>41 && i < 118 
-        [A,map] = imread(Filesb(i).name);
+    if i>42 && i < 119 
+        [A,map] = imread(char(Filesb(i,1).name));
         newVector=reshape(A,[size(A,1)*size(A,2)*size(A,3), 1]);
-        newcoffee_mug= [newcoffee_mug;newVector];
+        newcoffee_mug= [newcoffee_mug,newVector];
     end
     %elements of soda_can
-    if i>117 && i < 186 
-        [A,map] = imread(Filesb(i).name);
+    if i>118 && i < 187 
+        [A,map] = imread(char(Filesb(i,1).name));
         newVector=reshape(A,[size(A,1)*size(A,2)*size(A,3), 1]);
-        newsoda_can= [newsoda_can;newVector];
+        newsoda_soda_can= [newsoda_soda_can,newVector];
+    end
+    %bowl
+    if i>186 && i < 297
+        [A,map] = imread(char(Filesb(i,1).name));
+        newVector=reshape(A,[size(A,1)*size(A,2)*size(A,3), 1]);
+        newsoda_bowl= [newsoda_bowl,newVector];
+    end
+    %flashlight
+    if i>296 && i < 427
+        [A,map] = imread(char(Filesb(i,1).name));
+        newVector=reshape(A,[size(A,1)*size(A,2)*size(A,3), 1]);
+        newsoda_flashlight= [newsoda_flashlight,newVector];
     end
     
 end
 
 
 %calculation of cov and mean for cap
-COV_cap=cov(newCAP);
-mu_cap = mean(newsoda_can);
+COV_cap=cov(double(newCAP));
+mu_cap = mean(newCAP);
 
 %calculation of cov for coffee_mug
-COV_coffee_mug=cov(newcoffee_mug);
-mu_coffee = mean(newsoda_can);
+COV_coffee_mug=cov(double(newcoffee_mug));
+mu_coffee = mean(newcoffee_mug);
 
 %calculation of cov for soda_can
-COV_soda_can=cov(newsoda_can);
-mu_soda_can = mean(newsoda_can);
+COV_soda_can=cov(double(newsoda_soda_can));
+mu_soda_can = mean(newsoda_soda_can);
+
+
+
+%calculation of cov for newsoda_bowl
+COV_soda_bowl=cov(double(newsoda_bowl));
+mu_soda_bowl = mean(newsoda_bowl);
+
+
+
+%calculation of cov for newsoda_bowl
+COV_soda_flashlight=cov(double(newsoda_flashlight));
+mu_soda_flashlight = mean(newsoda_flashlight);
+
 
 %features for GP
-
-
-
-
-
