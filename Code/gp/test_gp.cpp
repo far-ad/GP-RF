0322f591621f8c5c055d3a873da01c1ea02ca228
Jack1102 <dragomir.nikolic1987@gmail.com>
2015-06-12 16:43:25 +0200

    Adding CMatrix *read_rgbd_data_cmat

---
+++ Code/gp/test_gp.cpp
@@ -0,0 +1,28 @@
+#include <CGp.h>
+
+#include <iostream>
+#include <CMatrix.h>
+
+int main(int argc, char** argv) {
+	std::cout << "GP running!" << std::endl;
+
+	CMatrix *TESTING, *TRAINING;
+	const char* filenameTESTING= "adding training path";
+	const char* filenameTRAINING= "adding testing path";
+
+	TESTING= read_rgbd_data_cmat( filenameTESTING );
+	TRAINING= read_rgbd_data_cmat( filenameTRAINING );
+
+	CGp *gp = new CGp();
+
+}
+
+//loading training and testing sets
+CMatrix *read_rgbd_data_cmat( const char* filename, int n_samples=0 )
+{
+	int rows;
+	int cols;
+	double *dataset = read_rgbd_data( filename, &rows, &cols, n_samples );
+
+	return new CMatrix(rows, cols, dataset);
+}
