476858aaf30363b720f39ca876ea84547b3e2374
Raphael Dümig <duemig@in.tum.de>
2015-06-16 16:23:02 +0200

    random forest: correct initialization of vector 'var_type'
    var_type is a vector that describes which attribute/feature is numeric,
    categorical, etc…
    Its size is (1 x #features+1). The first #features elements contain the
    information about the features/attributes, the last element contains the
    information about the label.
    In our case all the features/attributes are numeric, while the label is
    categorical.

--- Code/randomforest/test_rgbd.cpp
+++ Code/randomforest/test_rgbd.cpp
@@ -29,7 +29,7 @@ int main(int argc, char** argv)
 
   cv::Mat var_type = cv::Mat(training_data.cols + 1, 1, CV_8U );
   var_type.setTo(cv::Scalar(CV_VAR_NUMERICAL) ); // all inputs are numerical
-  var_type.at<uchar>(14000, 0) = CV_VAR_CATEGORICAL;
+  var_type.at<uchar>(training_data.cols, 0) = CV_VAR_CATEGORICAL;
 
   /********************************步骤1：定义初始化Random Trees的参数******************************/
   float priors[] = {1,1,1,1,1};  // weights of each classification for classes
@@ -70,8 +70,8 @@ int main(int argc, char** argv)
       // train on the testing data:
       // test_sample = training_data.row(tsample);
       /********************************步骤3：预测*********************************************/
-      result = rtree->predict(test_sample, cv::Mat());
-      label = (int) testing_labels.at<float>(tsample, 0);
+      result = (int) rtree->predict(test_sample, cv::Mat());
+      label  = (int) testing_labels.at<float>(tsample, 0);
       
       printf("Testing Sample %i -> class result (digit %d) - label (digit %d)\n", tsample, result, label);
       
