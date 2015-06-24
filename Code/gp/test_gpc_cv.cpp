b5705b21bb8055ec110d62c9ee035d91e2bfdda0
Raphael Dümig <duemig@in.tum.de>
2015-06-24 17:59:44 +0200

    gp classifier: optional command line argument
    The maximum number of samples to load can be passed on the command line
    after the file paths to the data.

--- Code/gp/test_gpc_cv.cpp
+++ Code/gp/test_gpc_cv.cpp
@@ -3,6 +3,8 @@
 #include "gpc.hpp"
 
 #include <iostream>
+#include <sstream>
+#include <cstdlib>
 
 void fvec2dvec(float* vec, double* res_vec, int n)
 {
@@ -14,10 +16,23 @@ void fvec2dvec(float* vec, double* res_vec, int n)
 
 int main(int argc, char** argv) {
 
-	cv::Mat training_data = read_rgbd_data_cv(argv[1]);
-	cv::Mat training_labels = read_rgbd_data_cv(argv[2]);
-	cv::Mat testing_data = read_rgbd_data_cv(argv[3]);
-	cv::Mat testing_labels = read_rgbd_data_cv(argv[4]);
+	if(argc < 5) {
+		std::cout << "please provide training data/labels and testing data/labels as arguments!" << std::endl;
+		exit(1);
+	}
+
+	int max_samples = 0;
+
+	// use fifth argument as maximum number of samples if the argument is present
+	if(argc > 5) {
+		istringstream(argv[5]) >> max_samples;
+	}
+
+	// read the input data into cv matrices to demonstrate the conversion to double*
+	cv::Mat training_data = read_rgbd_data_cv(argv[1], max_samples);
+	cv::Mat training_labels = read_rgbd_data_cv(argv[2], max_samples);
+	cv::Mat testing_data = read_rgbd_data_cv(argv[3], max_samples);
+	cv::Mat testing_labels = read_rgbd_data_cv(argv[4], max_samples);
 
 	int input_dim = training_data.cols;
 	int n_training_data = training_data.rows;
@@ -28,6 +43,9 @@ int main(int argc, char** argv) {
 	double* testing_data_d = new double[n_testing_data*input_dim];
 	double* testing_labels_d = new double[n_testing_data*input_dim];
 
+	// for now we just copy the content to a different vector for conversion
+	// The better approach would be to keep the opencv matrices as doubles, s.t.
+	// we can take their data directly
 	fvec2dvec((float*) training_data.datastart, training_data_d, n_training_data);
 	fvec2dvec((float*) training_labels.datastart, training_labels_d, n_training_data);
 	fvec2dvec((float*) testing_data.datastart, testing_data_d, n_testing_data);
