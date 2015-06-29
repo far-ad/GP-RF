7dab995337bc8e636c0ed3f947356da9ba05e609
DJLoco <andreaswie@gmail.com>
2015-06-29 19:06:34 +0200

    rf: get_leaf_nodes - fixed traversing the tree

--- Code/randomforest/get_leaf_nodes.cpp
+++ Code/randomforest/get_leaf_nodes.cpp
@@ -1,68 +1,23 @@
-#include <opencv/cv.h>
-#include <opencv/ml.h>
+#include "get_leaf_nodes.hpp"
 #include <stdio.h>
 #include <iostream>
-#include <list>
 #include <deque>
 
-using namespace std;
-using namespace cv;
-
-list<const CvDTreeNode*> get_leaf_node( CvForestTree* tree )
+leaf_list& get_leaf_node( CvForestTree* tree )
 {
-	//const CvForestTree* tree = tree;
-    std::list<const CvDTreeNode*> leaf_list;
-    std::deque<const CvDTreeNode*> test_nodes;//used to find the nodes that needed to be delete from right_nodes
+    leaf_list* leaf_list = new ::leaf_list;
     std::deque<const CvDTreeNode*> right_nodes;
-	const CvDTreeNode* node = tree->get_root();
-	const CvDTreeNode* temp = node;
-	const CvDTreeNode* tmp = node;
-	right_nodes.push_back(node);
+    const CvDTreeNode* node = tree->get_root();
+
+    right_nodes.push_back(node);
 	
-	while (!right_nodes.empty()) {
+    while (!right_nodes.empty()) {
 
-		for (node = right_nodes.back(), tmp = node; node->left != NULL; node = node->left) {
-			
-			right_nodes.push_back(node->right);
-   		}
-		temp = right_nodes.back();
-        leaf_list.insert (leaf_list.begin(), temp);	
-		leaf_list.insert (leaf_list.begin(), node);
-
-		right_nodes.pop_back();//pop the node which is always on the end
-
-		if (right_nodes.back() == tmp) //pop the other node, but first need to find it
-		{
-			right_nodes.pop_back(); 
-			printf("the node you deleted is at the back.\n");
-		}
-            
-		else if (right_nodes.front() == tmp)
-			{
-				right_nodes.pop_front();
-				printf("the node you deleted is at the front.\n");
-			}
-			else 
-			{   
-				while (right_nodes.front() != tmp)
-				{
-					test_nodes.push_front(right_nodes.front());
-					right_nodes.pop_front();
-				}
-				right_nodes.pop_front();
-				printf("the node you deleted is in the middle of deque.\n");
-				while (!test_nodes.empty())
-				{
-					right_nodes.push_front(test_nodes.front());
-					test_nodes.pop_front();
-				}
-			}
+	for (node = right_nodes.back(), right_nodes.pop_back(); node->left != NULL; node = node->left) {
+	    right_nodes.push_back(node->right);
 	}
+	leaf_list->push_front(node);
+    }
      
-    return leaf_list;
+    return *leaf_list;
 }
-
-
-
-
-
